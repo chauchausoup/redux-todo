d2644a833982baaa307515dda47dd8e2e359b6d0
Krishna <7krishna7dhakal7@gmail.com>
2020-10-28 09:25:10 +0545

    wtf error

--- redux/fetchFromApi.js
+++ redux/fetchFromApi.js
@@ -2,6 +2,9 @@
 
 const redux = require("redux");
 const axios = require('axios')
+const thunkMiddleware = require('redux-thunk').default
+const applyMiddleware=redux.applyMiddleware
+
 
 const createStore = redux.createStore;
 
@@ -9,7 +12,6 @@ const createStore = redux.createStore;
 const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
 const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
 const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
-const API_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
 
 
 function fetchUserRequest() {
@@ -18,15 +20,17 @@ function fetchUserRequest() {
     info: "lets fetch users from our api to store the value in our state object",
   };
 }
-function fetchUserSuccess() {
+function fetchUserSuccess(users) {
     return {
       type: FETCH_USERS_SUCCESS,
+      payload:users,
       info: "lets fetch users succeded",
     };
 }
-function fetchUserFailure() {
+function fetchUserFailure(error) {
     return {
       type: FETCH_USERS_FAILURE,
+      payload:error,
       info: "lets fetch users failed",
     };
 }
@@ -36,39 +40,53 @@ function fetchUserFailure() {
 //now we will implement a reducer
 
 const initial_user_state={
-    loading:true,
-    users:axios(),
+    loading:false,
+    users:[],
     error:""
 }
 
 
-const fetchUserRequestReducer = (state = initial_user_state, action) => {
+const reducer = (state = initial_user_state, action) => {
   switch (action.type) {
     case FETCH_USERS_REQUEST:
       return {
         ...state,
         loading:true,
-        users:,
+        
       };
-    default:
-      return state;
-  }
+    case FETCH_USERS_SUCCESS:
+      return{
+        ...state,
+        loading:false,
+        users:action.payload,
+        error:''
+        
+      };
+    case FETCH_USERS_FAILURE:
+      return{
+        ...state,
+        loading:false,
+        users:[],
+        error:action.payload
+      }
+ 
 };
 
+const fetchUsers=()=>{
+  return function(dispatch){
+    dispatch(fetchUserRequest())
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response=>{
+        const users=response.data.map(user=>user.id)
+        dispatch(fetchUserSuccess(users))
+      })
+      .catch(error=>{
+        dispatch(fetchUserFailure(error.message))
+      })
+  }
+}
 
-const rootReducer = redux.combineReducers({
-  requestUsers:fetchUserRequestReducer
-});
 
-const store = createStore(rootReducer);
-console.log("Initial State:", store.getState());
-
-const unsubscribe = store.subscribe(() =>
-  console.log("Update state", store.getState())
-);
-
-store.dispatch(buyIceCream());
-store.dispatch(buyCake());
-store.dispatch(buyIceCream());
-
-unsubscribe();
+const store = createStore(reducer,applyMiddleware(thunkMiddleware));
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchUsers())
